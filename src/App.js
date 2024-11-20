9a034b82a55503d7a2440badc0351d68d26e8234
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-11-20 20:54:42 -0300

    Aplicando o estilo Inline

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ function App() {
       <h1>React com CSS</h1>
       <MyComponent />
       <p>Este parágrafo é do App.js</p>
-  
+      {/*Inline CSS*/}
+      <p style={{ color: 'blue', padding: '25px', borderTop: "5px solid red" }}>
+        Este elemento foi estilizado de forma inline...
+      </p>
+      <p style={{ color: 'magenta', padding: '25px', borderTop: "5px solid gold" }}>
+        Este elemento foi estilizado de forma inline...
+      </p>
+      
     </div>
   );
 }
